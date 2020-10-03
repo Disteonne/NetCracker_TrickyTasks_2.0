5dbed8a288ff4e61f6b56d7149e217b71d45a235
Disteonne <diana.razmikovna@yandex.ru>
2020-10-03 19:23:41 +0300

    tasks vers 0

---
+++ src/main/java/TaskOne/IMatrixWorkerIpl.java
@@ -0,0 +1,52 @@
+package TaskOne;
+
+public class IMatrixWorkerIpl implements IMatrixWorker {
+    public IMatrixWorkerIpl() {
+        super();
+    }
+
+    public void print(double[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean haveSameDimension(double[][] m1, double[][] m2) {
+        return (m1.length == m2.length && m1[0].length == m2[0].length) ? true : false;
+    }
+
+    public double[][] add(double[][] m1, double[][] m2) {
+        double[][] tmp_matrix=new double[m1.length][m1[0].length];
+        if(haveSameDimension(m1,m2)==true) {
+            for (int i = 0; i < m1.length; i++) {
+                for (int j = 0; j < m1[0].length; j++) {
+                tmp_matrix[i][j]=m1[i][j]+m2[i][j];
+                }
+            }
+            return tmp_matrix;
+        }
+        else
+            throw new IllegalStateException();
+    }
+
+    public double[][] subtract(double[][] m1, double[][] m2) {
+        double[][] tmp_matrix=new double[m1.length][m1[0].length];
+        if(haveSameDimension(m1,m2)==true) {
+            for (int i = 0; i < m1.length; i++) {
+                for (int j = 0; j < m1[0].length; j++) {
+                    tmp_matrix[i][j]=m1[i][j]-m2[i][j];
+                }
+            }
+            return tmp_matrix;
+        }
+        else
+            throw new IllegalStateException();
+    }
+
+    public double[][] multiply(double[][] m1, double[][] m2) {
+        return new double[0][];
+    }
+}
---
+++ src/test/java/TaskOne/IMatrixWorkerIplTest.java
@@ -0,0 +1,30 @@
+package TaskOne;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IMatrixWorkerIplTest {
+    private double[][] matrix_one={{1,2,3},{4,5,6},{7,8,9}};
+    private double[][] matrix_two={{2,7,7},{4,5,1},{9,6,2}};
+
+    @Test
+    public void haveSameDimension() {
+        IMatrixWorker iMatrixWorker=new IMatrixWorkerIpl();
+        boolean b=iMatrixWorker.haveSameDimension(matrix_one,matrix_two);
+        assertEquals(true,iMatrixWorker.haveSameDimension(matrix_one,matrix_two));
+    }
+    @Test
+    public void testAddMatrix(){
+        IMatrixWorker iMatrixWorker=new IMatrixWorkerIpl();
+        double[][] res={{3,9,10},{8,10,7},{16,14,11}};
+        assertArrayEquals(res,iMatrixWorker.add(matrix_one,matrix_two));
+    }
+    @Test
+    public void testSubtract(){
+        IMatrixWorker iMatrixWorker=new IMatrixWorkerIpl();
+        double[][] res={{-1,-5,-4},{0,0,5},{-2,2,7}};
+        assertArrayEquals(res,iMatrixWorker.subtract(matrix_one,matrix_two));
+    }
+}
